69e6da5b9b52460bd57c6976b3be789ac937a28d
ajb85 <ajbrush85@gmail.com>
2019-03-26 15:50:18 -0400

    Added post request for api/zoos

--- zoo/routes.js
+++ zoo/routes.js
@@ -20,4 +20,23 @@ routes.get("/", async (req, res) => {
   }
 });
 
+routes.post("/", async (req, res) => {
+  if (req.body.hasOwnProperty("name") && req.body.name.toString().length > 0) {
+    try {
+      const id = await db.insert(req.body).into("zoos");
+      const newZoo = await db("zoos")
+        .where({ id: id[0] })
+        .first();
+      res.status(201).json(newZoo);
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "There was an error adding that to the databaes." });
+    }
+  } else {
+    status(400).json({ message: "Please include a name with the entry" });
+  }
+});
+
 module.exports = routes;
